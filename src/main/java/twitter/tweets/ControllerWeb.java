bda27d7ad7b66910c99b0cd7a7212ad111d2223e
alwaysintilte <bropapkag2k@gmail.com>
2025-03-20 17:33:51 +0300

    added connection between front and back
    now when u load a page a request from server is sent to the page and image is insta changed

--- src/main/java/twitter/tweets/ControllerWeb.java
+++ src/main/java/twitter/tweets/ControllerWeb.java
@@ -3,32 +3,32 @@ package twitter.tweets;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.IOException;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+
+import static twitter.tweets.tweetAnalyzer.calculateAverageSentiments;
+import static twitter.tweets.tweetAnalyzer.groupTweetsByState;
 
 @RestController
 public class ControllerWeb {
-//    @GetMapping("/getsentiments")
-//    public Map<String, Double> getfromamount(String fromCurrency, String toCurrency, double amount, double convertedamount){
-//        Map<String, Double> response = new HashMap<>();
-//        CurrencyRequest currencyRequestFrom = new CurrencyRequest(fromCurrency, amount);
-//        CurrencyRequest currencyRequestTo = new CurrencyRequest(toCurrency, convertedamount);
-//        for (Currency currency : currenciesList) {
-//            if(currency.getAbbreviation().equals(currencyRequestTo.getCurrency())){
-//                currencyRequestTo.setId(currency.getId());
-//                currencyRequestTo.setAmountToBYN(currency.getAmount());
-//            }
-//            if(currency.getAbbreviation().equals(currencyRequestFrom.getCurrency())){
-//                currencyRequestFrom.setId(currency.getId());
-//                currencyRequestFrom.setAmountToBYN(currency.getAmount());
-//            }
-//        }
-//        currencyRequestFrom.setRateToBYN(getRequestedCurrencyRate(currencyRequestFrom.getId()));
-//        currencyRequestTo.setRateToBYN(getRequestedCurrencyRate(currencyRequestTo.getId()));
-//        double toRate=(currencyRequestFrom.getRateToBYN()*currencyRequestTo.getAmountToBYN())/
-//                (currencyRequestTo.getRateToBYN()*currencyRequestFrom.getAmountToBYN());
-//        convertedamount=amount*toRate;
-//        response.put("amount", convertedamount);
-//        return response;
-//    }
+    @GetMapping("/getsentimentsonload")
+    public Map<String, Double> getSentimentsOnLoad() throws IOException {
+        Map<String, State> states = JsonMethods.JsonDeserializer("src/main/resources/static/states.json");
+        List<Tweet> tweets = Arrays.asList(
+                new Tweet(-118.2437, 34.0522, "Love the weather in LA!", 0.9), // California
+                new Tweet(-74.0060, 40.7128, "New York is amazing!", -0.4), // New York
+                new Tweet(-95.3698, 29.7604, "Texas BBQ is the best!", 0.1), // Texas
+                new Tweet(-80.1918, 25.7617, "Miami beach is awesome! 1", -0.8), // Florida
+                new Tweet(-85.497, 30.997536, "Miami beach is awesome! 2", -0.2), // Florida
+                new Tweet(-80.2918, 25.6617, "Miami beach is awesome! 3", 0.3), // Florida
+                new Tweet(-81.1918, 25.7617, "Miami beach is awesome! 4", 0.5), // Florida
+                new Tweet(-155.886774, 19.248084, "Hi lol 1", 0.0), // Hi
+                new Tweet(-155.886774, 19.258084, "Hi lol 2", null) // Hi
+        );
+        Map<String, List<Tweet>> groupedTweets = groupTweetsByState(tweets, states);
+        return calculateAverageSentiments(groupedTweets);
+    }
 }
